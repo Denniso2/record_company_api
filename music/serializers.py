f71223a53a370d066a9e8b74523989cc31803fba
Dionysios Polyzos <dennis12552@gmail.com>
2022-08-18 01:30:13 +0300

    Validate duration input

--- music/serializers.py
+++ music/serializers.py
@@ -1,11 +1,20 @@
 from rest_framework import serializers
+import time
 from .models import Artist, Album, Track
+
 
 
 class TrackSerializer(serializers.ModelSerializer):
     class Meta:
         model = Track
         fields = ['id', 'title', 'lyrics', 'track_duration', 'album', 'artist']
+
+    def validate_track_duration(self, value):
+        try:
+            time.strptime(value, '%M:%S')
+            return value
+        except ValueError:
+            raise serializers.ValidationError("Incorrect duration format")
 
 
 class ArtistSerializer(serializers.ModelSerializer):
@@ -15,20 +24,23 @@ class ArtistSerializer(serializers.ModelSerializer):
 
 
 class AlbumSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Album
-        fields = ['id', 'title', 'album_type', 'description', 'artist', 'tracks']
-
     album_type = serializers.SerializerMethodField(
         method_name='select_album_type'
     )
 
+    class Meta:
+        model = Album
+        fields = ['id', 'title', 'album_type', 'description', 'artist', 'tracks']
+
     def select_album_type(self, album: Album):
-        if not album.tracks_count:
+        try:
+            if not album.tracks_count:
+                return
+            if album.tracks_count == 1:
+                return 'Single'
+            elif album.tracks_count <= 3:
+                return 'EP'
+            else:
+                return 'Album'
+        except AttributeError:
             return
-        if album.tracks_count == 1:
-            return 'Single'
-        elif album.tracks_count <= 3:
-            return 'EP'
-        else:
-            return 'Album'
